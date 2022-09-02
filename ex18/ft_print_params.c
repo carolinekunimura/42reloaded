e3fe0adfec962355e1e1460acdf2234bb4f72641
Caroline Kunimura <carolinekunimura@gmail.com>
2022-09-02 23:22:02 +0200

    all

--- ex18/ft_print_params.c
+++ ex18/ft_print_params.c
@@ -6,33 +6,28 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 21:05:12 by coder             #+#    #+#             */
-/*   Updated: 2022/09/02 22:23:32 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/02 23:20:34 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char j)
-{
-	write(1, &j, 1);
-}
+void	ft_putchar(char c);
 
 int	main(int argc, char **argv)
 {
 	int	i;
-	int j;
-	
-	i = 0;
-	j = 0;
-	while(i < argc)
+	int	j;
+
+	i = 1;
+	while (i < argc)
 	{
-		while(argv[j] != "\0")
+		j = 0;
+		while (argv[i][j] != '\0')
 		{
-		ft_putchar(*argv[j]);
-		j++;
+			ft_putchar(argv[i][j]);
+			j++;
 		}
-	ft_putchar('\n');
-	i++;
+		ft_putchar('\n');
+		i++;
 	}
-	return(0);
+	return (0);
 }
